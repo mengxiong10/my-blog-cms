6c90317d131997b7048f0b8e86ab80acef0ebfa5
Mengxiong10 <15623530292@163.com>
2017-05-10 19:19:34 +0800

    完善 mock

--- src/server/mock.js
+++ src/server/mock.js
@@ -15,6 +15,23 @@ Mock.mock(/\/article\/list/, {
   "total": 200
 })
 
+Mock.mock(/\/article\/\d+/, {
+  "data": {
+    "_id": "@id",
+    "title": "@ctitle",
+    "content": "@cparagraph(250)",
+    "created_at": "@datetime",
+    "comment_count|1-200": 1,
+    "visit_count|1-200": 1,
+    "status|1": [0, 1],
+    "tags|1-2": [{ name: "@word(2)" }]
+  }
+})
+
+Mock.mock(/\/article/, 'post', 'ok')
+Mock.mock(/\/article\/\d+/, 'put', 'ok')
+Mock.mock(/\/article\/\d+/, 'delete', 'ok')
+
 Mock.mock(/\/tag\/list/, {
   "data|6": [{
     "_id": "@id",
